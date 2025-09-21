2b43e1adafec08c62334443f7a4a4c70f0d8b6c1
admin <Anderson Balena Fernandes>
2025-09-21 20:24:50 +0000

    Fix create user endpoint.

--- src/main/java/com/abfsistemas/ecommerce_api/controller/UserController.java
+++ src/main/java/com/abfsistemas/ecommerce_api/controller/UserController.java
@@ -6,17 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.abfsistemas.ecommerce_api.model.User;
 import com.abfsistemas.ecommerce_api.repository.IUserRepository;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/users")
 public class UserController {
     
     @Autowired
     IUserRepository userRepository;
 
-    @PostMapping
+    @PostMapping("/")
     public ResponseEntity create(@RequestBody User body) {
         User user = userRepository.findByUsername(body.getUsername());
         if (user != null) {
